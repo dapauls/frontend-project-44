59dc38229802e6200742efb2d496e9d19f69f6ef
darya <dabespalova@mail.ru>
2022-11-05 00:06:59 +0400

    игра-прогрессия

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -2,13 +2,20 @@
 
 import readlineSync from 'readline-sync';
 
+const randomNumber = (min, max) => {
+  const r = Math.random() * (max - min) + min;
+  return Math.floor(r);
+};
+
+const randomIndex = () => randomNumber(0, 7);
+
 const getProgression = () => {
   let firstNumber = Math.round(Math.random() * 100);
   const stepOfProgression = Math.round(Math.random() * 10);
-  const lengthOfRowWithoutFirstBumber = 6;
+  const lengthOfRowWithoutFirstNumber = 6;
   const numbers = [];
   numbers.push(firstNumber);
-  for (let i = 0; i < lengthOfRowWithoutFirstBumber; i += 1) {
+  for (let i = 0; i < lengthOfRowWithoutFirstNumber; i += 1) {
     const lastNumber = firstNumber + stepOfProgression;
     numbers.push(lastNumber);
     firstNumber = lastNumber;
@@ -16,14 +23,11 @@ const getProgression = () => {
   return numbers;
 };
 
-const getHiddenProgression = (numbers) => {
-  let index;
-  for (let i = 0; !numbers.includes(index); i += 1) {
-    index = Math.round(Math.random() * 10);
-  }
-  const hiddenNumber = numbers[index];
-  numbers[index] = '..';
-  return numbers;
+const rightAnswer = (pro, num) => pro[num];
+
+const getHiddenProgression = (array, randomHid) => {
+  array[randomHid] = '.. ';
+  return array;
 };
 
 const game = () => {
@@ -31,7 +35,24 @@ const game = () => {
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hi, ${userName}!`);
   for (let i = 0; i <= 2; i += 1) {
-    
+    const hidden = randomIndex();
+    const progression = getProgression();
+    const progressionWithoutHidden = progression;
+    const correctAnswer = rightAnswer(progressionWithoutHidden, hidden);
+    const hiddenProgression = getHiddenProgression(progression, hidden);
+    const question = `Question: ${hiddenProgression.join(', ')}`;
+    console.log(question);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer === correctAnswer.toString()) {
+      console.log('Correct!');
+    } else {
+      console.log(question);
+      console.log(`Your answer: ${userAnswer}`);
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
   }
+  console.log(`Congratulations, ${userName}!`);
 };
 game();
